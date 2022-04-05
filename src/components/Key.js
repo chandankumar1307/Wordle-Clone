f9aed03b0f6978dd8f413267c31e2340e905ce33
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2022-04-05 11:36:17 +0530

    onclik functionality done

---
+++ src/components/Key.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+
+export const Key = ({ keyVal, bigKey }) => {
+  const { onSelectLetter, onDelete, onEnter } = useContext(AppContext);
+
+  const selectLetter = () => {
+    if (keyVal === "ENTER") {
+      onEnter();
+    } else if (keyVal === "DELETE") {
+      onDelete();
+    } else {
+      onSelectLetter(keyVal);
+    }
+  };
+
+  return (
+    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
+      {keyVal}
+    </div>
+  );
+};
